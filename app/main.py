085d4231bd78d96de577b6b057320c5a7598bd90
Nik <nikita.pitushkin31@mail.com>
2026-04-06 13:05:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
 
     elif cat_age >= 24:
         human_age = (
-            (2 + (cat_age - 24) //4)
+            (2 + (cat_age - 24) // 4)
         )
         cat_and_dog_in_human_years.append(human_age)
 
@@ -17,7 +17,7 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
         cat_and_dog_in_human_years.append(1)
     elif dog_age >= 24:
         human_age = (
-                2 + (dog_age - 24) // 5
+            (2 + (dog_age - 24) // 5)
         )
         cat_and_dog_in_human_years.append(human_age)
 
--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,29 @@
 from app.main import get_human_age
 
 
-def test_return_zero_years():
-    assert  get_human_age(0, 0) == [0, 0]
+def test_return_zero_years() -> None:
+    assert get_human_age(0, 0) == [0, 0]
 
 
-def test_return_less_than_15():
+def test_return_less_than_15() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_return_15():
+def test_return_15() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_return_less_than_24():
+def test_return_less_than_24() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
 
-def test_return_24():
+def test_return_24() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
 
-def test_cat_dog_age_after_24_years():
+def test_cat_dog_age_after_24_years() -> None:
     assert get_human_age(27, 27) == [2, 2]
 
 
-def test_cat_and_dog_return_different_result():
+def test_cat_and_dog_return_different_result() -> None:
     assert get_human_age(28, 28) == [3, 2]
